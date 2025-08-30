389a83bbf866bf857c4cba16706ad4f6a60ac70e
sinyorlang-design <sinyorlang@gmail.com>
2025-08-30 12:50:57 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Tradeline App is Live!</h1>
+      <p>If you see this, your Vercel deployment worked 🎉</p>
+    </div>
+  );
+}
