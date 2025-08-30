abdb047aed00e784043618c5d9e83e2744812687
sinyorlang-design <sinyorlang@gmail.com>
2025-08-30 13:11:35 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{maxWidth: 720, margin: "60px auto", fontFamily: "system-ui, sans-serif", lineHeight: 1.5}}>
+      <h1 style={{fontSize: 36, marginBottom: 12}}>🚀 TradeLine 24/7</h1>
+      <p>Deployment check page. If you can read this, Vercel is working.</p>
+    </div>
+  );
+}
